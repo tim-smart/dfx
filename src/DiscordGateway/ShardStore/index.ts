5b24301a3785d4b299e74a2d69e3ea1a82f35d73
Tim Smart <tim.smart@arisechurch.com>
2022-12-03 12:47:48 +1300

    feat: ShardStore

---
+++ src/DiscordGateway/ShardStore/index.ts
@@ -0,0 +1,32 @@
+import { none } from "@fp-ts/data/Option"
+
+export interface ClaimIdContext {
+  sharderCount: number
+  totalCount: number
+}
+
+export interface ShardStore {
+  claimId: (ctx: ClaimIdContext) => Effect<never, never, Maybe<number>>
+  allClaimed: (totalCount: number) => Effect<never, never, boolean>
+  heartbeat?: (shardId: number) => Effect<never, never, void>
+}
+export const ShardStore = Tag<ShardStore>()
+
+// Very basic shard id store, that does no health checks
+const memoryStore = (): ShardStore => {
+  let currentId = 0
+
+  return {
+    claimId: ({ totalCount }) =>
+      Effect.sync(() => {
+        if (currentId >= totalCount) return none
+        const id = currentId
+        currentId++
+        return Maybe.some(id)
+      }),
+
+    allClaimed: (totalCount) => Effect.sync(() => currentId >= totalCount),
+  }
+}
+
+export const LiveMemoryShardStore = Layer.sync(ShardStore)(memoryStore)
