e86948dff233feb9a1d6b73817f406654fe6f8fd
Tim <hello@timsmart.co>
2023-05-28 14:08:49 +1200

    feat: webhook makeLiveWithoutConfig

--- src/webhooks.ts
+++ src/webhooks.ts
@@ -36,3 +36,15 @@ export const makeLiveWithoutFetch = (
 export const makeLive = (
   config: Config.Wrap<DiscordConfig.MakeOpts & MakeConfigOpts>,
 ) => LiveFetchRequestExecutor >> makeLiveWithoutFetch(config)
+
+export const makeLiveWithoutConfig = (
+  options: DiscordConfig.MakeOpts & MakeConfigOpts,
+) => {
+  const config = DiscordConfig.make(options)
+  const LiveConfig = Layer.succeed(DiscordConfig.DiscordConfig, config)
+  const LiveWebhook = makeConfigLayer(options)
+  const LiveLog = config.debug ? Log.LiveLogDebug : Log.LiveLog
+  const LiveEnv =
+    (LiveLog + LiveConfig) >> (MemoryREST + LiveWebhook + MemoryRateLimit)
+  return LiveFetchRequestExecutor >> LiveEnv
+}
