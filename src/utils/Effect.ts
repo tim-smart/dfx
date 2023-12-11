cd1287d588da2d8e824e8c856c749565393306ac
Tim <hello@timsmart.co>
2023-12-11 16:29:15 +1300

    refactor: subscribeForEachPar / foreverSwitch

--- src/utils/Effect.ts
+++ src/utils/Effect.ts
@@ -4,7 +4,6 @@ import * as Effect from "effect/Effect"
 import type * as Fiber from "effect/Fiber"
 import * as PubSub from "effect/PubSub"
 import * as Queue from "effect/Queue"
-import * as ScopedRef from "effect/ScopedRef"
 
 export const subscribeForEachPar = <R, E, A, X>(
   self: PubSub.PubSub<A>,
@@ -16,7 +15,7 @@ export const subscribeForEachPar = <R, E, A, X>(
       Effect.flatMap(queue =>
         Effect.forever(
           Effect.flatMap(Queue.take(queue), _ =>
-            Effect.forkScoped(
+            Effect.fork(
               Effect.catchAllCause(effect(_), _ =>
                 Deferred.failCause(deferred, _),
               ),
@@ -38,23 +37,26 @@ export const foreverSwitch = <R, E, A, R1, E1, X>(
   f: (_: A) => Effect.Effect<R1, E1, X>,
 ): Effect.Effect<R | R1, E | E1, never> =>
   pipe(
-    Effect.all([
-      Deferred.make<E1, never>(),
-      ScopedRef.fromAcquire<R1, never, Fiber.RuntimeFiber<unknown, unknown>>(
-        Effect.fork(Effect.unit),
-      ),
-    ]),
-    Effect.flatMap(([causeDeferred, fiberRef]) => {
-      const run = Effect.forever(
-        Effect.flatMap(self, _ =>
-          ScopedRef.set(
-            fiberRef,
-            Effect.forkScoped(
-              Effect.tapErrorCause(f(_), _ =>
-                Deferred.failCause(causeDeferred, _),
-              ),
-            ),
+    Effect.all([Deferred.make<E1, never>(), Effect.fiberId]),
+    Effect.flatMap(([causeDeferred, fiberId]) => {
+      let fiber: Fiber.RuntimeFiber<unknown, unknown> | undefined
+
+      const run = pipe(
+        self,
+        Effect.tap(() =>
+          fiber ? fiber.interruptAsFork(fiberId) : Effect.unit,
+        ),
+        Effect.flatMap(_ =>
+          pipe(
+            f(_),
+            Effect.tapErrorCause(_ => Deferred.failCause(causeDeferred, _)),
+            Effect.fork,
           ),
+        ),
+        Effect.tap(fiber_ =>
+          Effect.sync(() => {
+            fiber = fiber_
+          }),
         ),
       )
 
@@ -63,5 +65,4 @@ export const foreverSwitch = <R, E, A, R1, E1, X>(
         discard: true,
       }) as Effect.Effect<R | R1, E | E1, never>
     }),
-    Effect.scoped,
   )
